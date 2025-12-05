2482f0dc51e65d60583d9b6015212d90647c493d
pashikantirakes <rakeshpashikanti740@gmail.com>
2025-12-05 07:07:45 +0530

    add extra endpoint

--- src/main/java/com/example/sample_jenkins/SampleJenkins.java
+++ src/main/java/com/example/sample_jenkins/SampleJenkins.java
@@ -1,6 +1,7 @@
 package com.example.sample_jenkins;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class SampleJenkins {
     @GetMapping("/hi")
     public String sayHi(){
         return "Hi Jenkins";
+    }
+
+    @GetMapping("/hi/{name")
+    public String greeting(@PathVariable String name){
+        return "Hi Jenkins - " + name;
     }
 }
